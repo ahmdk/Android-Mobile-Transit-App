3e823ab8d961012884213f965bea2979598cb425
Ahmed Khalifa <mostafa.jr@gmail.com>
2016-05-10 18:13:37 -0700

    Adjusted line spacing

--- BranchStringParser.java
+++ BranchStringParser.java
@@ -20,8 +20,8 @@ public class BranchStringParser {
      */
     public static List<LatLon> parseBranch(String branch) {
         //list of lat/lon objects that store location data
-		//input string similar to [[[0.093493,51.6037],[0.091015,51.5956],[0.088596,51.5857],...]]
-		List<LatLon> listOfLocations = new ArrayList<LatLon>();
+	//input string similar to [[[0.093493,51.6037],[0.091015,51.5956],[0.088596,51.5857],...]]
+	List<LatLon> listOfLocations = new ArrayList<LatLon>();
         String[] listOfStrings;
         double lat = 0;
         double lon = 0;
@@ -31,21 +31,21 @@ public class BranchStringParser {
 
             for (int i = 0; i < listOfStrings.length; i++){
                 //remove brackets from input Tfl branch string 
-				String noBrackets = listOfStrings[i].replaceAll("\\[|\\]", "");
+		String noBrackets = listOfStrings[i].replaceAll("\\[|\\]", "");
                 listOfStrings[i].trim();
 				
-				//seperate latitude and longitude data for each station
+		//seperate latitude and longitude data for each station
                 String[] latLonPointsString =  noBrackets.split(",");
                 for (int j = 0; j < latLonPointsString.length; j++){
                     if (j % 2 == 0){
                         //convert string value to a double
-						lon = Double.parseDouble(latLonPointsString[j]);
+			lon = Double.parseDouble(latLonPointsString[j]);
                     }
                     else
                         lat = Double.parseDouble(latLonPointsString[j]);
                 }
 				
-				//create a new LatLon object that contains 				
+		//create a new LatLon object that contains 				
                 LatLon branchPoint = new LatLon(lat, lon);
                 listOfLocations.add(branchPoint);
             }
